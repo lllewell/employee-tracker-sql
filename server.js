cf74707ca91f72bb7da508aa2be4eaff4569e3f0
Alien <lmllewellyn@gmail.com>
2024-07-29 21:49:22 -0400

    fixed functions, working on update function

--- server.js
+++ server.js
@@ -43,29 +43,53 @@ const addDepartment = () => {
 };
 
 const addRole = () => {
-  return prompt({
-    message: 'What role would you like to add?',
-    name: 'role',
-  },
-  {
-    message: 'What is the salary for this role?',
-    name: 'salary',
-  },
-  {
-    message: 'What is the department number for this role?',
-    name: 'department'
-  }).then((answer) => {
-    return client.query(`INSERT INTO roles (title, salary, department_id) VALUES ($1)`, [answer.role, answer.salary, answer.department]);
+  return prompt([
+    {
+      message: 'What role would you like to add?',
+      name: 'role',
+    },
+    {
+      message: 'What is the salary for this role?',
+      name: 'salary',
+    },
+    {
+      message: 'What is the department number for this role?',
+      name: 'department'
+    }
+  ]).then((answer) => {
+    return client.query(`INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)`, [answer.role, answer.salary, answer.department]);
   })
 };
 
-const addEmployee = (employee) => {
-  return client.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES $1`, [first_name, last_name, role_id, manager_id]);
+const addEmployee = () => {
+  return prompt([
+    {
+      message: 'What role would you like to add?',
+      name: 'role',
+    },
+    {
+      message: 'What is the salary for this role?',
+      name: 'salary',
+    },
+    {
+      message: 'What is the department number for this role?',
+      name: 'department'
+    }
+  ]).then((answer) => {
+    return client.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`, [answer.first_name, answer.last_name, answer.role_id, answer.manager_id]);
+  })
 };
 
 const updateEmployee = () => {
-  let updatedRow = (answer.role_id)
-  return client.query(`UPDATE FROM employee WHERE id = $1`, [updatedRow]);
+  return prompt([
+    {
+      message: 'Which employee would you like to update?',
+      name: 'updateName',
+      choices: []
+    }
+  ]).then((answer) => {
+    return client.query(`UPDATE FROM employee WHERE id = $1`, [answer.updateName]);
+  })
 
 };
 
@@ -99,9 +123,13 @@ const init = () => {
       } else if (answer.type === 'View All Employees') {
         return viewAllEmployees().then(logTable).then(init)
       } else if (answer.type === 'Add Department') {
-        return addDepartment().then(init)
+        return addDepartment().then(logTable).then(init)
       } else if (answer.type === 'Add Role') {
-        return addRole().then(init)
+        return addRole().then(logTable).then(init)
+      } else if (answer.type === 'Add Employee') {
+        return addEmployee().then(logTable).then(init)
+      } else if (answer.type === 'Update Employee') {
+        return updateEmployee().then(logTable).then(init)
       } else {
         process.exit();
       }
